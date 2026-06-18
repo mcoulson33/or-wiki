6df6a76dd8f0fbce6b2c0d3cd6a32f65f800e4a8
Matt Coulson <47127731+mcoulson33@users.noreply.github.com>
2026-06-18 14:36:32 -0500

    ...

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,3 +2,12 @@ export default function(eleventyConfig) {
   // Copies the entire 'css' folder to the output directory
   eleventyConfig.addPassthroughCopy("css");
 };
+
+module.exports = function(eleventyConfig) {
+  return {
+    dir: {
+      input: "src",
+      output: "_site"
+    }
+  };
+};
